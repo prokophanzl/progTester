c8e937ed4310926086dfdd0affc52ec44812ba03
Prokop Hanzl <prokophanzl666@gmail.com>
2021-12-24 00:53:00 +0100

    Add licence information

--- progtester.sh
+++ progtester.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# progTester: a test script for ProgTest
+# Copyright (C) 2021 Prokop Hanzl
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 PROG=$1
 # DIR=$2
 DIR=${2%/}

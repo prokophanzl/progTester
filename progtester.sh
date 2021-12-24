3c13d9474b24e231abea49a75f3a0cbab8bea655
Prokop Hanzl <prokophanzl666@gmail.com>
2021-12-24 13:05:39 +0100

    Update version number

--- progtester.sh
+++ progtester.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-VERSION='0.2.0'
+VERSION='0.3.0'
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
